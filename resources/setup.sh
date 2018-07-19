76422f8d81522de4fe967795edf742f33939538e
Nick Houghton <nhoughto@gmail.com>
2018-07-19 21:43:08 +1000

    Update README with Private Registry Details Fix setup.sh script and pod health, closes #1

--- resources/setup.sh
+++ resources/setup.sh
@@ -99,9 +99,10 @@ while true; do
     fi
 
     echo "Checking startup status.."
-    POD_STATUSES=$(kubectl get po -n kube-system -o jsonpath='{.items[*].status.containerStatuses[*].ready}' | tr ' ' '\n' | sort | uniq)
-    POD_COUNT=$(kubectl get po -n kube-system | wc -l)
-    if [ "$POD_STATUSES" == "true" ] && [ $POD_COUNT -gt 7 ]; then
+    POD_STATES=$(kubectl get po -n kube-system -o jsonpath='{.items[*].status.containerStatuses[*].state}' | tr ' ' '\n' | cut -d'[' -f 2 | cut -d':' -f 1 | sort | uniq)
+    POD_READINESS=$(kubectl get po -n kube-system -o jsonpath='{.items[*].status.containerStatuses[*].ready}' | tr ' ' '\n' | sort | uniq)
+    POD_COUNT=$(kubectl get po -n kube-system --no-headers | wc -l)
+    if [ "$POD_READINESS" == "true" ] && [ "$POD_STATES" == "running" ] && [ $POD_COUNT -gt 6 ]; then
         echo "startup successful"
         break
     fi

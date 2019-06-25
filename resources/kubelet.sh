732137281ea68c3b2cf7e41f6147082b310caa08
Nick Houghton <nhoughto@gmail.com>
2019-06-25 19:32:53 +1000

    Add 1.15

--- resources/kubelet.sh
+++ resources/kubelet.sh
@@ -4,7 +4,6 @@ sleep 2
 				 --cluster-domain=cluster.local \
 				 --bootstrap-kubeconfig=/etc/kubernetes/bootstrap-kubelet.conf \
 				 --pod-manifest-path=/etc/kubernetes/manifests \
-				 --allow-privileged=true \
 				 --cluster-dns=10.96.0.10 \
 				 --authorization-mode=Webhook \
 				 --client-ca-file=/var/lib/localkube/certs/ca.crt \

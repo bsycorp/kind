5595ec226a8db934d8b2ec317ab7cfb2a4df0d52
Clint Beacock <clint.beacock@gmail.com>
2020-02-06 10:37:10 -0500

    WIP: first successful build with latest docker

--- build.sh
+++ build.sh
@@ -5,17 +5,17 @@ TAG_LATEST="$1"
 TAG_VERSION="$2"
 
 if [ -z "$DOCKER_IMAGE" ]; then
-	DOCKER_IMAGE="18.06-dind"
+	DOCKER_IMAGE="19.03.5-dind"
 	echo "Defaulting Docker image to $DOCKER_IMAGE"
 fi
 
 if [ -z "$MINIKUBE_VERSION" ]; then
-	MINIKUBE_VERSION="v0.30.0"
+	MINIKUBE_VERSION="v1.0.1"
 	echo "Defaulting Minikube version to $MINIKUBE_VERSION"
 fi
 
 if [ -z "$KUBERNETES_VERSION" ]; then
-	KUBERNETES_VERSION="v1.10.5"
+	KUBERNETES_VERSION="v1.15.1"
 	echo "Defaulting Kubernetes version to $KUBERNETES_VERSION"
 fi
 
@@ -39,7 +39,8 @@ trap finish EXIT
 set -e
 
 echo "Starting dind"
-CONTAINER_ID=$(docker run --network $NETWORK --privileged -d --rm docker:$DOCKER_IMAGE)
+CONTAINER_ID=$(docker run -e DOCKER_TLS_CERTDIR=/certs --network $NETWORK --privileged -d --rm docker:$DOCKER_IMAGE)
+echo $CONTAINER_ID
 docker cp resources/entrypoint.sh $CONTAINER_ID:/entrypoint.sh
 docker cp resources/setup.sh $CONTAINER_ID:/setup.sh
 docker cp resources/start.sh $CONTAINER_ID:/start.sh
@@ -50,6 +51,7 @@ docker cp before-cluster.sh $CONTAINER_ID:/before-cluster.sh
 docker cp after-cluster.sh $CONTAINER_ID:/after-cluster.sh
 
 echo "Starting setup"
+sleep 3
 docker exec $CONTAINER_ID /setup.sh $KUBERNETES_VERSION $MINIKUBE_VERSION $STATIC_IP
 echo "Commiting new container"
 docker commit \

1c90e6d3b526074ef1713519d2821454b2bf8d0f
Nick Houghton <nhoughto@gmail.com>
2021-02-10 12:03:52 +1100

    Add option to set node capacity to the detected limits of the kind instance

--- resources/kubelet.sh
+++ resources/kubelet.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 sleep 2
+
+# by default kind will set capacity to be the capacity of the host, and not honour any limits that have been imposed on it
+# this tries to reserve an amount inverse to the limit on kind to fix that
+if [ ! -z "ENFORCE_CAPACITY" ]; then
+	MEMORY_TOTAL=$(cat /proc/meminfo | grep MemTotal | awk '{print $2}')
+	MEMORY_LIMIT=$(($(cat /sys/fs/cgroup/memory/memory.limit_in_bytes) / 1024))
+	MEMORY_OUTSIDE_LIMIT=$(($MEMORY_TOTAL - $MEMORY_LIMIT))
+	echo "Memory to reserve to ensure correct node capacity: $MEMORY_OUTSIDE_LIMIT"
+
+	CPU_TOTAL=$(grep -c processor /proc/cpuinfo)
+	CPU_LIMIT=$(($(cat /sys/fs/cgroup/cpu/cpu.cfs_quota_us) / $(cat /sys/fs/cgroup/cpu/cpu.cfs_period_us)))
+	CPU_OUTSIDE_LIMIT=$(($CPU_TOTAL - $CPU_LIMIT))
+	echo "CPU to reserve to ensure correct node capacity: $CPU_OUTSIDE_LIMIT"
+
+	SYSTEM_RESERVED="--system-reserved=cpu=$CPU_OUTSIDE_LIMIT,memory=${MEMORY_OUTSIDE_LIMIT}Ki"
+	echo "Using arguments: $SYSTEM_RESERVED"
+fi
 /usr/bin/kubelet --hostname-override=minikube \
+                 $SYSTEM_RESERVED \
 				 --cluster-domain=cluster.local \
 				 --bootstrap-kubeconfig=/etc/kubernetes/bootstrap-kubelet.conf \
 				 --pod-manifest-path=/etc/kubernetes/manifests \
